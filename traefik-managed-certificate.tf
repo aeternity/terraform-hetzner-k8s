6c8c0c8ac03b73f9c4e03f2d6e3b917f41f4179c
Petar Peshev <petar.peshev@leanplum.com>
2022-07-04 23:25:17 +0300

    some fixes in traefik cert

--- traefik-managed-certificate.tf
+++ traefik-managed-certificate.tf
@@ -4,14 +4,10 @@ terraform {
       source  = "hetznercloud/hcloud"
       version = "~> 1.0"
     }
-    hetznerdns = {
-      source  = "timohirt/hetznerdns"
-      version = "2.1.0"
-    }
   }
 }
 
 resource "hcloud_managed_certificate" "main" {
   name         = "${local.env_human}.superk8s.de"
-  domain_names = ["*.${local.env_human}.superk8s.de", "${local.env_human}.superk8s.de"]
+  domain_names = ["*.${local.env_human}.superk8s.de", "superk8s.de"]
 }
